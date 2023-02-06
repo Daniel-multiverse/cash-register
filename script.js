8d95f14faf5914643eeb3bf01cdac19b4eda5d99
MustafaAldelaimi <mustafa1007@yahoo.com>
2023-02-06 16:17:41 +0000

    Added comments to explain code

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ function cashRegister(price, cash, CID){
 
   //initial variables declared below
 
-  let changeDue = Math.round((cash - price) * 100)    // change due multiplied by 100 to avoid decimal issues
+  let changeDue = Math.round((cash - price) * 100)    // changeDue multiplied by 100 to avoid decimal issues
   const totalChangeDue = changeDue    //reference value for the total change due
   let changeGiven = 0   //tracks sum of change due to be given
 
-  //object below containing values to be updated further down the code
+  //object below in desired format containing values to be updated further down the code
 
   let result = {status: "",
                 change: [["PENNY", 0],
@@ -20,33 +20,34 @@ function cashRegister(price, cash, CID){
                 ["ONE HUNDRED", 0]]
               }
   
-  //for loop below calculates total cash in drawer and assigns it to totalCID
-  
+  //for loop below calculates total cash in drawer and assigns it to totalCID (total cash in drawer)
+
   let totalCID = 0
   for (let i = 0; i < CID.length; i++) {
-    totalCID += Math.round((CID[i][1]) * 100)
+    totalCID += Math.round((CID[i][1]) * 100)   //values scaled by 100 to avoid decimal issues
   }
 
-  // if the cash is less than the price then the result status becomes 'INCORRECT_PAYMENT and 
-  // the result change is returned empty
+ 
   if (cash < price){
     return {status: "INCORRECT_PAYMENT", change: []}
-    //if cash is equal to price, the result status is changed to CLOSED
-  } else if (cash === price) {
+  }
+    //if cash is equal to price, the result status is changed to CLOSED and the CID is returned 
+   else if (cash === price) {
     result.status = "CLOSED"
     return result
+
+    /* if the total amount of money in drawer(CID) is less than the changed due then the result status is 
+    INSUFFICIENT_FUNDS and change is returned
+    */
   }else if(totalCID<changeDue){
     return {status: "INSUFFICIENT_FUNDS", change: []}
   }else if(totalCID === changeDue){
-    return {status: "CLOSED", change: CID} //make sure it return the change
+    return {status: "CLOSED", change: CID} 
   }else{
-    result.status = 'OPEN'
-    // Updates the result status to open
+    result.status = 'OPEN'    // Updates the result status to open
   }  
-  
-  //array below contains currency values multiplied by 100
 
-  let moneyCompare = [1, 5, 10, 25, 100, 500, 1000, 2000, 10000]
+  let moneyCompare = [1, 5, 10, 25, 100, 500, 1000, 2000, 10000]    //array contains currency values multiplied by 100
 
   //for loop below increments through each 1D array in the 2D arrays (CID and the "change" property)
   //[1] selects the money in the 1D arrays
@@ -60,10 +61,10 @@ function cashRegister(price, cash, CID){
     //Loop breaks when the selected currency value in the drawer is empty or no more of it is needed to add to change
 
     while (CID[i][1] >= moneyCompare[i] && changeDue >= moneyCompare[i]) {
-      result.change[i][1] += (moneyCompare[i] / 100);
-      changeDue -= moneyCompare[i];
-      changeGiven += moneyCompare[i]  // adds currency value to "changeGiven" every cycle
-      CID[i][1] -= moneyCompare[i];
+      result.change[i][1] += (moneyCompare[i] / 100);   //  adds currency value to it's corresponding currency in the array in the "change" property of the result object
+      changeDue -= moneyCompare[i];   //  adds currency value to "changeDue" every cycle
+      changeGiven += moneyCompare[i]  //  adds currency value to "changeGiven" every cycle
+      CID[i][1] -= moneyCompare[i];   //  removes currency value from the CID every cycle
     }
   }
 
@@ -84,7 +85,7 @@ function cashRegister(price, cash, CID){
   
 
   // Example function call
-  console.log(cashRegister(10, 15.1, [
+  cashRegister(10, 15.1, [
     ["PENNY", 1.01],
     ["NICKEL", 2.05],
     ["DIME", 3.1],
@@ -94,4 +95,4 @@ function cashRegister(price, cash, CID){
     ["TEN", 20],
     ["TWENTY", 60],
     ["ONE HUNDRED", 100],
-  ]));
+  ]);
